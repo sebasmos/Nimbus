bf3b592efe662f71d54aed03cc8f054a821be3d4
sebasmos <ulsordonez@unicauca.edu.co>
2021-06-25 11:08:14 +0200

    updating website

--- website/src/components/Layout/MainLayout.js
+++ website/src/components/Layout/MainLayout.js
@@ -31,7 +31,7 @@ class MainLayout extends React.Component {
 
       this.notificationSystem.addNotification({
         title: <MdImportantDevices />,
-        message: 'Welome to Reduction Admin!',
+        message: 'Welome to Nimbus Admin!',
         level: 'info',
       });
     }, 1500);
@@ -44,7 +44,7 @@ class MainLayout extends React.Component {
       this.notificationSystem.addNotification({
         title: <MdLoyalty />,
         message:
-          'Reduction is carefully designed template powered by React and Bootstrap4!',
+          'Nimbus is carefully designed template powered by React and Bootstrap4!',
         level: 'info',
       });
     }, 2500);
--- website/src/components/Layout/Sidebar.js
+++ website/src/components/Layout/Sidebar.js
@@ -121,7 +121,7 @@ class Sidebar extends React.Component {
                 alt=""
               />
               <span className="text-white">
-                Reduction <FaGithub />
+                Nimbus <FaGithub />
               </span>
             </SourceLink>
           </Navbar>
